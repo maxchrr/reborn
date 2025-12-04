8022608e41933cec2ea3e19eeb10d85f085027f2
Max Charrier <max@puffer.fish>
2025-12-04 18:38:21 +0100

    Add sixthsence
    Signed-off-by: Max Charrier <max@puffer.fish>

--- spells/Spell.java
+++ spells/Spell.java
@@ -1,6 +1,11 @@
 package spells;
 
+import java.util.List;
+
 import core.Hero;
+import items.Item;
+import locations.Exit;
+import locations.LocationBase;
 
 public class Spell {
 	private String name;
@@ -38,16 +43,57 @@ public class Spell {
 	}
 	
 	public void action(Hero hero, Spell spell) {
-		hero.useMana(spell.cost);
 		switch(spell.type) {
 		case BUBBLES:
 			hero.getWriter().display("｡.°˚∘˙○˚.•｡˚𓈒𓏸.°•○");
 			break;
 		case HEAL:
-			hero.addHealth(value);
+			hero.addHealth(this.value);
 			break;
 		case SIXTHSENCE:
-			// show one of the exits
+			LocationBase currentLocation = hero.getLocation();
+			
+			// List of all accessible and not hidden exits
+			List<Exit> exits = currentLocation.getExits().values()
+					.stream()
+					.filter(e -> e.isAccessible() && !e.isHidden())
+					.toList();
+			
+			if (exits.isEmpty()) {
+				 hero.getWriter().display("Your vision sees nothing... no exits here.");
+				 break;
+			}
+			
+			// Random selector
+			Exit randomExit = exits.get((int)(Math.random() * exits.size()));
+			LocationBase targetLocation = randomExit.getTarget();
+			
+			hero.getWriter().display("You remotely view: " + targetLocation.getName());
+			
+			// Items
+			if (targetLocation.getItems().isEmpty()) {
+		        hero.getWriter().display(" Items: none");
+		    } else {
+		        hero.getWriter().display(" Items:");
+		        for (Item item : targetLocation.getItems()) {
+		            hero.getWriter().display("  - " + item.getName());
+		        }
+		    }
+			
+			// Character
+			if (targetLocation.hasCharacter()) {
+		        hero.getWriter().display("\n Character: " + targetLocation.getCharacter().getName());
+		    } else {
+		        hero.getWriter().display(" Character: none");
+		    }
+
+			// Exits
+		    hero.getWriter().display("\n Exits:");
+		    for (Exit e : targetLocation.getExits().values()) {
+		        if (e.isAccessible()) {
+		            hero.getWriter().display("  - " + e.getTarget().getName());
+		        }
+		    }
 			break;
 		case RADIOCALL:
 			// show a list of Locations between you and the spaceship
